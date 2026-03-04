e3a9c51d798ba9ff1976525545af09a9f018dbb6
5s-mini <dnclsehd123@naver.com>
2026-03-04 11:52:15 +0900

    feat: Member 엔티티 추가, Member 샘플 데이터 5개 생성

--- src/main/java/com/back/P23260303Application.java
+++ src/main/java/com/back/P23260303Application.java
@@ -2,12 +2,13 @@ package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class P23260303Application {
 
     public static void main(String[] args) {
         SpringApplication.run(P23260303Application.class, args);
     }
-
 }
